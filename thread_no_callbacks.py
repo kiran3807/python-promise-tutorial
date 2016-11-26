cd6d1b06632de9603c799dc8d52c974a775c16ae
Kiran <kiran384707@gmail.com>
2016-11-27 02:51:23 +0530

    modified thread no cllback, made it more instructive

--- thread_no_callbacks.py
+++ thread_no_callbacks.py
@@ -11,20 +11,43 @@ def get_data(post_id,data):
         req = u.Request(site,headers=hdr)
         raw_data = u.urlopen(req).read()
         time.sleep(4)
-        print "data received -- " + str(len(data) )
-        data.append(json.loads(raw_data)['title'] )
+        print "data received -- " + str(post_id)
+        """ Setting the flag and populating the data at the same time """
+        data[post_id] = json.loads(raw_data)['title']
         
     my_thread = t.Thread(target=__get_data)
     my_thread.start()
     
+def handle_data_1():
+    print "1 has been handled"
+    
+def handle_data_2():
+    print "2 has been handled"
+    
+def handle_data_3():
+    print "3 has been handled"
+    
+
 def boss_thread():
-    data = []
+    """ We use data dict as a collection of flag as it is shared among the threads """
+    data = {}
     current = 0
     while len(data) < 3:
         if len(data) == current:
             get_data(current+1, data)
             current += 1
         time.sleep(2)
+        """ These are the handlers for the data retreived in the worker threads"""
+        if 1 in data and data[1]:
+            handle_data_1()
+            data[1] = False
+        if 2 in data and data[2]:
+            handle_data_2()
+            data[2] = False
+        if 3 in data and data[2]:
+            handle_data_3()
+            data[2] = False
+            
         print "processing"
     
     
